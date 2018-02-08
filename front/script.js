35c031b976adbcc0f7dad738444d5b98810e8bf5
Suissa <jnascimento@gmail.com>
2018-02-07 22:44:45 -0200

    front

--- front/script.js
+++ front/script.js
@@ -1,16 +1,17 @@
-const Lib = {};
+// const Lib = {};
 
-const _get = (params) => {
+// const _get = (url = 'http://localhost:3000/products') => {
+  
+//   return ajax({
+//     method: 'get',
+//     url
+//   })
+// }
 
-}
+// Lib.get = _get
 
-var request = ajax({
-  method: 'get',
-  url: 'http://localhost:3000/products'
-})
-
-const success = (data) => console.log('success: ', data)
-const error = (err) => console.log('error: ', err)
+// const success = (data) => console.log('success: ', data)
+// const error = (err) => console.log('error: ', err)
 
 
-console.log(request.then(success).catch(error));
+// console.log(Lib.get('http://localhost:3000/products').then(success).catch(error))
