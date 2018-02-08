cdea026a2f6cad14005d4f4561ef1d3d5dfb4dcd
Suissa <jnascimento@gmail.com>
2018-02-07 22:15:16 -0200

    CORS OK

--- api-mocked/app.js
+++ api-mocked/app.js
@@ -24,7 +24,18 @@ app.use( bodyParser.urlencoded( { extended: false } ) )
 app.use( cookieParser() )
 app.use( express.static( path.join( __dirname, 'public' ) ) )
 
-app.use(cors())
+// app.use(cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // allow preflight
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 app.use( '/', index )
 app.use( '/products', products )
